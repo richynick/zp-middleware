23d0c973ef3d9f11e5b615a5ff4da508afc3659b
Richard Nnaji <richard.nnaji013@gmail.com>
2024-07-23 09:25:36 +0100

    comments on API-Key Been Hardcoded

--- src/main/java/com/richard/zptask/service/AuthenticationService.java
+++ src/main/java/com/richard/zptask/service/AuthenticationService.java
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class AuthenticationService {
+ /**  This API key will not be hardcoded here on a typical system, it is usually stored somewhere (s3 bucket or some other db),
+    and retrieve to check if it matches the API-key coming from requests Header*/
+
     private static final String AUTH_TOKEN_HEADER_NAME = "X-API-KEY";
     private static final String AUTH_TOKEN = "14DFB1BA-063C-43D9-8D5C-3020B1D9782D";
 
